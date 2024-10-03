84470c69254a3aa270c45293a31aa35095b537d5
adnene <bendiouisadnane3@gmail.com>
2024-10-03 23:26:52 +0100

    quick fix

--- randomNumberGame/script.js
+++ randomNumberGame/script.js
@@ -9,21 +9,17 @@ let number;
 let score = 0;
 easyButton.onclick = function () {
   randomnum = Math.floor(Math.random() * 10) + 1;
-  console.log(randomnum);
 }
 medieumButton.onclick = function () {
     randomnum = Math.floor(Math.random() * 50) + 1;
-    console.log(randomnum);
   }
 hardButton.onclick = function () {
     randomnum = Math.floor(Math.random() * 100) + 1;
-    console.log(randomnum);
   }
 MySubmit.onclick = function () {
     number = inputGuess.value;
     number = Number(number);
     if (number === randomnum) {
-      console.log(`"you guessed right !!"`),
       alert("you guessed right !!");
     }
     else {
