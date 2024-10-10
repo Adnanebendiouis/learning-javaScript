9f166afd63a724c256047ec64c5a7573828900cb
adnene <bendiouisadnane3@gmail.com>
2024-10-10 18:22:09 +0100

    created a random password generator using HTML CSS and javaScript

--- randomPasswordGenerator/script.js
+++ randomPasswordGenerator/script.js
@@ -1,27 +1,55 @@
-function generatePassword(length,includelowercase,includeuppercase,includenumbers,includesymbols){
-    let password = '';
-    let charSet = '';
-    if(includelowercase){
-        charSet += 'abcdefghijklmnopqrstuvwxyz';
+function generatePassword(
+    length,
+    includelowercase,
+    includeuppercase,
+    includenumbers,
+    includesymbols
+  ) {
+    if(!includelowercase && !includeuppercase && !includenumbers && !includesymbols){
+        alert("please include a type");
     }
-    if(includeuppercase){
-        charSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let password = "";
+    let charSet = "";
+    if (includelowercase) {
+      charSet += "abcdefghijklmnopqrstuvwxyz";
     }
-    if(includenumbers){
-        charSet += '0123456789';
+    if (includeuppercase) {
+      charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-    if(includesymbols){
-        charSet += '!@#$%^&*()_+{}|:<>?';
+    if (includenumbers) {
+      charSet += "0123456789";
     }
-    for(let i = 0; i < length; i++){
-        password += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    if (includesymbols) {
+      charSet += "!@#$%^&*()_+{}|:<>?";
+    }
+    for (let i = 0; i < length; i++) {
+      password += charSet.charAt(Math.floor(Math.random() * charSet.length));
     }
     return password;
-}
-const passwordLen = 12;
-const includelowercase = false;
-const includeuppercase = false;
-const includenumbers = false;
-const includesymbols = false;
-const password = generatePassword(passwordLen,includelowercase,includeuppercase,includenumbers,includesymbols);
-console.log(password);
+  }
+  
+  const passwordLen = 12;
+  const numbers = document.getElementById("numbers");
+  const uppercase = document.getElementById("uppercase");
+  const lowercase = document.getElementById("lowercase");
+  const specialcharacteres = document.getElementById("specialcharacteres");
+  const display = document.getElementById("password");
+  const button = document.getElementById("submit");
+  
+  button.onclick = function () {
+    const includelowercase = lowercase.checked;
+    const includeuppercase = uppercase.checked;
+    const includenumbers = numbers.checked;
+    const includesymbols = specialcharacteres.checked;
+  
+    const password = generatePassword(
+      passwordLen,
+      includelowercase,
+      includeuppercase,
+      includenumbers,
+      includesymbols
+    );
+  
+display.value = password;
+    console.log(password);
+  };
